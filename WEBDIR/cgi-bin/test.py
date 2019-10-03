2b1a17c70917ca2fa98e97aa45a44ed706b34a21
Rennka <rennka08.31@gmail.com>
2019-10-03 17:34:06 +0900

    lec3

---
+++ WEBDIR/cgi-bin/test.py
@@ -0,0 +1,15 @@
+import sys
+import io
+import datetime
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+#! /usr/bin/env python3
+print("Content-type: text/html\n")
+print("<html>")
+print(" <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head>")
+print(" <body>")
+print(  "システムアーキテクトプログラミング演習")
+print(  "<br>")
+print(   datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print(" </body>")
+print("</html>")
