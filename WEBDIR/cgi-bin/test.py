fc00c62bc10b167b57720e1258b674e984cdc92d
Rennka <rennka08.31@gmail.com>
2019-10-16 20:26:39 +0900

    formtest.htm書いた

--- WEBDIR/cgi-bin/test.py
+++ WEBDIR/cgi-bin/test.py
@@ -1,9 +1,9 @@
+#! /usr/bin/env python
 import sys
 import io
 import datetime
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-#! /usr/bin/env python3
 print("Content-type: text/html\n")
 print("<html>")
 print(" <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head>")
